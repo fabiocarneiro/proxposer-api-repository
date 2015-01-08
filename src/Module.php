bb3621708fd45e94264fe4782281666dbe5d4e7b
fabiocarneiro <fahecs@gmail.com>
2015-01-08 20:04:36 -0200

    add module dependency indicator

--- src/Module.php
+++ src/Module.php
@@ -3,9 +3,11 @@
 namespace ZFBrasil\Proxposer\Api\Repository;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 use ZF\Apigility\ApigilityModuleInterface;
+use ZFBrasil\Proxposer\Api;
 
-class Module implements ConfigProviderInterface, ApigilityModuleInterface
+class Module implements ConfigProviderInterface, DependencyIndicatorInterface, ApigilityModuleInterface
 {
     /**
      * {@inheritDoc}
@@ -13,5 +15,18 @@ class Module implements ConfigProviderInterface, ApigilityModuleInterface
     public function getConfig()
     {
         return require __DIR__ . '/../module.config.php';
+    }
+
+    /**
+     * Expected to return an array of modules on which the current one depends
+     * on
+     *
+     * @return array
+     */
+    public function getModuleDependencies()
+    {
+        return [
+            Api::class
+        ];
     }
 }
